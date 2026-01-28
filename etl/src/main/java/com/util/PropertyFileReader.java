ecc1dcdbab9b19ea64004f5f723f64b4057200c9
korkeep <korkeep@naver.com>
2026-01-28 21:22:26 +0900

    fix: package name

---
+++ etl/src/main/java/com/util/PropertyFileReader.java
@@ -0,0 +1,26 @@
+package com.util;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertyFileReader 
+{
+    public static Properties readPropertyFile(String file) throws IOException {
+        Properties props = new Properties();
+
+        try (InputStream input =
+                PropertyFileReader.class
+                    .getClassLoader()
+                    .getResourceAsStream(file)) {
+
+            if (input == null) {
+                throw new FileNotFoundException("Property file not found: " + file);
+            }
+            props.load(input);
+        }   
+        return props;
+    }
+}
+
