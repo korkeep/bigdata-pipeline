219feebfa1752f81054806809a46992c70ece1ef
korkeep <korkeep@naver.com>
2026-02-05 21:28:24 +0900

    feat: fetch FRED observation data and map to EtlColumnPojo

--- etl/src/main/java/com/processor/Fred2Hdfs.java
+++ etl/src/main/java/com/processor/Fred2Hdfs.java
@@ -1,14 +1,26 @@
 package com.processor;
 
 import java.net.URI;
+import java.net.URL;
+import java.util.List;
 import java.util.Properties;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
+import com.pojo.EtlColumnPojo;
+import com.pojo.FredColumnPojo;
 import com.util.PropertyFileReader;
+import com.util.US_STATES;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 
 public class Fred2Hdfs {
     
@@ -50,5 +62,56 @@ public class Fred2Hdfs {
 
         String namenode = fredProp.getProperty("hdfs.namenode.url");
         hadoopFs = FileSystem.get(new URI(namenode), conf);
+    }
+
+    public List<EtlColumnPojo> getEtlListData(FREQUENCY freq, US_STATES state, String searchText) throws Exception {
+        String fredUrl = fredProp.getProperty("fred.url");
+        String apiKey = fredProp.getProperty("fred.apiKey");
+        String fileType = "json";
+        String searchUrl = fredUrl + APITYPE.valueOf("SEARCH").apiType + "?search_text=" + searchText.replace(' ', '+') + 
+                            state.getName().replaceAll(" ", "+") + "&api_key=" + apiKey + "&file_type=" + fileType;
+        
+        System.out.println(searchUrl);
+    
+        JsonNode rootNode = mapper.readTree(new URL(searchUrl));
+        Thread.sleep(500);
+        ArrayNode nodeSeries = (ArrayNode)rootNode.get("series");
+        List<FredColumnPojo> listFredData = mapper.readValue(nodeSeries.traverse(), new TypeReference<List<FredColumnPojo>>(){});
+        Predicate<FredColumnPojo> predi = 
+            fred -> (
+                fred.getTitle().equals(searchText + state.getName()) &&
+                (fred.getFrequency_short().charAt(0) == freq.freq) &&
+                fred.getSeasonal_adjustment().equals("NSA")
+            );
+        List<EtlColumnPojo> listData = listFredData.stream().filter(predi).flatMap(pojo -> {
+            String observeUrl = fredUrl + APITYPE.valueOf("OBSERVATION").apiType +
+                                "?series_id=" + pojo.getId() + "&api_key=" + apiKey + "&file_type=" + fileType;
+    
+            System.out.println(observeUrl);
+    
+            try{
+                JsonNode nodeValue = mapper.readTree(new URL(observeUrl));
+                Thread.sleep(500);
+                ArrayNode nodeValueObserve = (ArrayNode)nodeValue.get("observations");
+                List<EtlColumnPojo> listEtlData = mapper.readValue(nodeValueObserve.traverse(), new TypeReference<List<EtlColumnPojo>>(){});
+    
+                for(EtlColumnPojo valuePojo : listEtlData){
+                    valuePojo.setState(state.getName());
+                    valuePojo.setId(pojo.getId());
+                    valuePojo.setTitle(pojo.getTitle().replace(',','_'));
+                    valuePojo.setFrequency_short(pojo.getFrequency_short());
+                    valuePojo.setUnits_short(pojo.getUnits_short());
+                    valuePojo.setSeasonal_adjustment_short(pojo.getSeasonal_adjustment_short());           
+                }
+                return listEtlData.stream();
+            }
+            catch(Exception e) {
+                e.printStackTrace();
+            }
+            return Stream.empty();
+            
+        }).collect(Collectors.toList());  
+        
+        return listData;
     }
 }
